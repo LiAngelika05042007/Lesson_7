7ddf7c903b035c45728d464377681c4bc15f1b10
LiAngelika05042007 <lanzelika884@gmail.com>
2023-05-13 16:26:33 +0600

    Initial commit

---
+++ src/Magic.java
@@ -0,0 +1,11 @@
+public class Magic extends Hero{
+    public Magic(String heroesAttackType) {
+        super(heroesAttackType);
+    }
+
+    @Override
+    public String superAbilityType() {
+        return "Magic применил суперспособность MAGICAL STRIKES";
+
+    }
+}
